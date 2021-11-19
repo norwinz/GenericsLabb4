6a67df7953941230ac4398d95104039d4be8f65a
norwinz <dan-gustafsson@hotmail.com>
2021-11-19 03:55:51 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsLabb4
+{
+    public class Employee
+    {
+        public int ID;
+        public string Name;
+        public string Gender;
+        public int Salary;
+
+        public Employee(int id, string name, string gender, int salary)
+            {
+            this.ID = id;
+            this.Name = name;
+            this.Gender = gender;
+            this.Salary = salary;
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsLabb4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                             //ID   NAME   GENDER  SALARY
+            Employee e1 = new Employee(101, "Anas", "Male", 20000);
+            Employee e2 = new Employee(102, "Hanna", "Female", 30000);
+            Employee e3 = new Employee(103, "Tobias", "Male", 40000);
+            Employee e4 = new Employee(104, "Sara", "Female", 40000);
+            Employee e5 = new Employee(105, "Anna", "Female", 50000);
+
+            Stack<Employee> EmployeeStack = new Stack<Employee>();
+            EmployeeStack.Push(e1);
+            EmployeeStack.Push(e2);
+            EmployeeStack.Push(e3);
+            EmployeeStack.Push(e4);
+            EmployeeStack.Push(e5);
+        
+        }
+    }
+}
